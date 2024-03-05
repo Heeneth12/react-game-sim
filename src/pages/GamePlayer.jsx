06bb5dd23b56bdea3d0115f0f98a44d2a690bd88
heeneth <heeneth123@gmail.com>
2024-03-05 21:19:13 +0530

    game view change

--- src/pages/GamePlayer.jsx
+++ src/pages/GamePlayer.jsx
@@ -6,19 +6,18 @@ function GamePlayer() {
   const { selectedGame } = useGameContext();
 
   return (
-    <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
-      {/* Left Advertisement */}
-      {/* Game iframe */}
-      <div className="flex-grow  md:mt-0 flex justify-center items-center">
-        <iframe
-          title="Selected Game"
-          src={selectedGame}
-          className="w-full h-96 md:h-full shadow-md"
-          allowFullScreen
-          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-        ></iframe>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
+        <div className="flex-grow mt-12 md:mt-0 flex justify-center items-center">
+          <iframe
+            title="Selected Game"
+            src={selectedGame}
+            className="w-full h-96 md:h-full shadow-md"
+            allowFullScreen
+            sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+          ></iframe>
+        </div>
       </div>
-      {/* Right Advertisement */}
     </div>
   );
 }

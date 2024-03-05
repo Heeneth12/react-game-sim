7ce7ddbaf8b904ce389221e5eb29b887881fc28b
heeneth <heeneth123@gmail.com>
2024-03-05 20:45:15 +0530

    removed all ads in games card and places in center

--- src/pages/GamePlayer.jsx
+++ src/pages/GamePlayer.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { useGameContext } from "../GameContext";
+import NavBar from "../components/NavBar";
 
 function GamePlayer() {
   const { selectedGame } = useGameContext();
 
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Your NavBar component */}
-
-      <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
-        {/* Left Advertisement */}
-
-        {/* Game iframe */}
-        <div className="flex-grow mt-12 md:mt-0 flex justify-center items-center">
-          <iframe
-            title="Selected Game"
-            src={selectedGame}
-            className="w-full h-96 md:h-full shadow-md"
-            allowFullScreen
-            sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-          ></iframe>
-        </div>
-
-        {/* Right Advertisement */}
+    <div className="flex flex-col md:flex-row flex-1 overflow-hidden">
+      {/* Left Advertisement */}
+      {/* Game iframe */}
+      <div className="flex-grow  md:mt-0 flex justify-center items-center">
+        <iframe
+          title="Selected Game"
+          src={selectedGame}
+          className="w-full h-96 md:h-full shadow-md"
+          allowFullScreen
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        ></iframe>
       </div>
+      {/* Right Advertisement */}
     </div>
   );
 }
